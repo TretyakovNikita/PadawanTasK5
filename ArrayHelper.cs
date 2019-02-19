76e759017f710988a01fccd51a16f5c536c988e4
Nikita Tret <accauntnikity@gmail.com>
2019-02-19 17:35:04 +0300

    Update ArrayHelper.cs

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -6,7 +6,14 @@ namespace PadawansTask5
     {
         public static string CheckIfSymmetric(int[] source)
         {
-           
+            if (source is null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (source.Length == 0)
+            {
+                throw new ArgumentException();
+            }
             for (int i = 0; i <= (source.Length - 1) / 2; i++)
             {
                 if (source[i] != source[source.Length - 1 - i])
